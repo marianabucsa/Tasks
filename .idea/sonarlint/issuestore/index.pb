e6b3e2d7b16945d54c7259f2a01f88150cb36cb1
marianabucsa <mary.bucsa00@yahoo.com>
2023-03-22 16:54:21 +0200

    Lab01

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+Z
+*src/main/resources/fxml/new-edit-task.fxml,6\a\6aeb24e3252900c930641e99bfb7ef468ea333e9
--- src/main/java/tasks/repository/ArrayTaskList.java
+++ src/main/java/tasks/repository/ArrayTaskList.java
@@ -53,7 +53,7 @@ public class ArrayTaskList extends TaskList {
 
     @Override
     public void add(Task task){
-        if (task.equals(null)) throw new NullPointerException("Task shouldn't be null");
+        if (task == null) throw new NullPointerException("Task shouldn't be null");
         if (numberOfTasks == currentCapacity-1){
             currentCapacity = currentCapacity * 2;
             Task[] withAddedTask = new Task[currentCapacity];
