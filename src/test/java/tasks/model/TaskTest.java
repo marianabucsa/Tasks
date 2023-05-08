cd29f4b8607dcf671d351a28c5b251d077e22151
Valentina Chioveanu <valentina.chioveanu@yahoo.com>
2023-05-08 19:44:34 +0300

    Lab04

--- src/test/java/tasks/model/TaskTest.java
+++ src/test/java/tasks/model/TaskTest.java
@@ -22,16 +22,10 @@ class TaskTest {
         Assertions.assertEquals(2, task.getRepeatInterval());
     }
 
-    @ParameterizedTest
-    @Tag("Create Task")
-    @DisplayName("Flow: Title error")
-    @CsvSource({"'',2023-04-01 12:00,2023-04-02 12:00,2",
-            "TEuPxtA5RxraXwN7Q69fpt2p27XGS5qz24Qq9ffGIwYf3phOUG3O62txHUhr8rd8WrCvR2BsKU4QqCUF8EqnhAuTWUm4Sc4p2vQlSWimKlUkBX0LDf2uUdI9au6zogmmLkBIUZwHVEmN0Z4oToafAA4WB7jF24aKpukaibhpnMd3XRpkxDryXe6UqskpkP2438925D4Vpf9qBBCBRACFrIJSFxWPwMr9iac7HBuPtbyEV2j2REpFnqR6EaMXwZ8Y" +
-                    ",2023-04-01 12:00,2023-04-02 12:00,2",})
-    void testCreateTaskTitleError(String title, String startTime, String endTime, int interval) throws ParseException {
+    @Test
+    void testCreateTaskTitleError() throws ParseException {
         try {
-            System.out.println(title);
-            Task task = new Task(title, Task.getDateFormat().parse(startTime), Task.getDateFormat().parse(endTime), interval);
+            Task task = new Task("", Task.getDateFormat().parse("2023-04-01 12:00"), Task.getDateFormat().parse("2023-04-02 12:00"), 2);
             assertTrue(false);
         } catch(IllegalArgumentException e) {
             System.out.println(e.getMessage());
@@ -39,29 +33,20 @@ class TaskTest {
         }
     }
 
-    @ParameterizedTest
-    @Tag("Create Task")
-    @DisplayName("Flow: Date error")
-    @CsvSource({"task,2023-04-02 12:00,2023-04-01 12:00,-3",
-            "task,2023-04-01 12:00,2023-04-01 12:00,2",
-            "task,2023-04-02 12:00,2023-04-01 12:00,2",})
-    void testCreateTaskDateError(String title, String startTime, String endTime, int interval) throws ParseException {
+    @Test
+    void testCreateTaskDateError() throws ParseException {
         try {
-            Task task = new Task(title, Task.getDateFormat().parse(startTime), Task.getDateFormat().parse(endTime), interval);
+            Task task = new Task("task", Task.getDateFormat().parse("2023-04-02 12:00"), Task.getDateFormat().parse("2023-04-01 12:00"), -3);
             assertTrue(false);
         } catch(IllegalArgumentException e) {
             assertTrue(e.getMessage().contains("Start date cannot be after end date"));
         }
     }
 
-    @ParameterizedTest
-    @Tag("Create Task")
-    @DisplayName("Flow: Interval error")
-    @CsvSource({"task,2023-04-02 12:00,2023-04-01 12:00,-3",
-            "task,2023-04-01 12:00,2023-04-02 12:00,0",})
-    void testCreateTaskIntervalError(String title, String startTime, String endTime, int interval) throws ParseException {
+    @Test
+    void testCreateTaskIntervalError() throws ParseException {
         try {
-            Task task = new Task(title, Task.getDateFormat().parse(startTime), Task.getDateFormat().parse(endTime), interval);
+            Task task = new Task("task", Task.getDateFormat().parse("2023-04-02 12:00"), Task.getDateFormat().parse("2023-04-01 12:00"), -3);
         }catch(IllegalArgumentException e) {
             assertTrue(e.getMessage().contains("Interval should be >= 1"));
         }
